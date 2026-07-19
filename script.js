dc595847c4041a3a726c2926bc3d142f982dc08a
dataapk <onlinedatabase.expert@gmail.com>
2026-07-19 21:44:45 +0600

    Refactor toggleProfileMenu function
    Refactor toggleProfileMenu to ensure notification is hidden before toggling profile menu.

--- script.js
+++ script.js
@@ -654,11 +654,29 @@ function toggleNotifMenu(event) {
 
 // প্রোফাইলের জন্য আলাদা ফাংশন
 function toggleProfileMenu(event) {
+
     event.stopPropagation();
+
     const menu = document.getElementById('profile-popup');
     const notif = document.getElementById('notif-popup');
-    notif.style.display = 'none'; // অন্য মেনু বন্ধ
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+
+
+    // Notification close
+    if(notif){
+        notif.style.display = 'none';
+    }
+
+
+    // Profile popup toggle
+    if(menu){
+
+        menu.style.display =
+        (menu.style.display === 'block')
+        ? 'none'
+        : 'block';
+
+    }
+
 }
 // ==============================
 // PROFILE SUB  START
